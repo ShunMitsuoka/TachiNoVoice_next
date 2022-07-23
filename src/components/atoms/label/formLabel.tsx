f31b250e55966d471282ca92840f3e2711d59c58
nikaidoshinya <105964115+nikaidoshinya@users.noreply.github.com>
2022-07-23 09:09:19 +0000

    足りないやつ

--- src/components/atoms/label/formLabel.tsx
+++ src/components/atoms/label/formLabel.tsx
@@ -3,17 +3,19 @@ import React from 'react';
 interface Props {
     children?: React.ReactNode;
     htmlFor: string;
+    id?: string;
     _class?: string;
 }
 
 export const FormLabel: React.FC<Props> = ({
     children,
     htmlFor,
+    id,
     _class = '',
 }) => {
     return (
         <div className={_class}>
-            <label htmlFor={htmlFor} className="text-xl font-bold">{children}</label>
+            <label htmlFor={htmlFor} id={id} className="text-xl font-bold">{children}</label>
         </div>
     );
 }
