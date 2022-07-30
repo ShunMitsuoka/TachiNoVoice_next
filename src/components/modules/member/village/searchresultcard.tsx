31a9810860bd5b0fa07fd337b43bbb72e6425e30
ozkoharu <78905667+ozkoharu@users.noreply.github.com>
2022-07-30 10:58:11 +0000

    search追加

---
+++ src/components/modules/member/village/searchresultcard.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import React from 'react';
+
+interface Props {
+    children?: React.ReactNode,
+    name?: string;
+    id?: string;
+    value1?: string,
+    value2?: string,
+    _class?: string,
+}
+
+export const SearchResultCard: React.FC<Props> = ({
+    children,
+    name,
+    id,
+    value1 = '',
+    value2 = '',
+    _class = '',
+}) => {
+    return (
+        <div className=' col-span-12 md:col-span-6 text-sub mt-5 px-6 text-center rounded-lg drop-shadow bg-white'>
+            <Link href={'/member/village/search/details/' + id}>
+                <a>
+                    <div className='text-3xl text-center font-bold'>{value1}</div>
+                    <div className='mt-4 scroll-m-9 h-24'>{value2}</div>
+                </a>
+            </Link>
+        </div >
+    );
+}
--- src/pages/member/village/search/index.tsx
+++ src/pages/member/village/search/index.tsx
@@ -9,11 +9,27 @@ import { getSession, useSession } from 'next-auth/react'
 import { AuthService } from '@/app/services/authService'
 import { ApiService } from '@/app/services/apiService'
 import { RouteManager } from '@/app/manages/routeManager'
+import { SearchResultCard } from '@/components/modules/member/village/searchresultcard'
 
 
 type formData = {
     keyword: string,
 };
+type formresultnum = {
+    resultnum: string,
+}
+// type responsetitle = {
+//     formtitle: string
+// }
+// type responsecontent = {
+//     formcontent: string
+// }
+
+type cardtype = {
+    id: "",
+    title: string,
+    content: string,
+}
 
 const Search: NextPage = () => {
     const { data: session, status } = useSession();
@@ -21,6 +37,9 @@ const Search: NextPage = () => {
     const [formkeyword, setKeyword] = useState<formData>({
         keyword: "",
     });
+
+    const [formcards, setCards] = useState<cardtype[]>([]);
+
 
     const changeHandler = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         setKeyword(prevValues => {
@@ -32,20 +51,29 @@ const Search: NextPage = () => {
         const params = {
             keyword: formkeyword.keyword,
         };
+
         const config = ApiService.getAuthHeader(session);
         ApiService.setConfig('params', params, config);
+        console.log(ApiService.setConfig('params', params, config));
+        console.log('aaaaaaaaaaaaaaaaaa');
+        console.log(ApiService.getFullURL(RouteManager.apiRoute.member.village.resource));
+        console.log('bbbbbbbbbbbbbbbbbbbbbbb');
+        console.log(ApiService.getFullURL(RouteManager.apiRoute.member.village.resource), {
+            params: {
+                keyword: formkeyword,
+            }
+        })
 
-        console.log(config);
         axios.get(ApiService.getFullURL(RouteManager.apiRoute.member.village.resource), config)
             .then(function (response) {
                 console.log('true');
-                console.log(response);
+                setCards(response.data.result);
             })
             .catch((error) => {
                 console.error(error);
             });
     }
-    //データをとってきて、とってきたデータ分のタイトルとcontentを表示する
+    //データをとってきて、とってきたデータ分のタイトルとcontentを表示するß
 
     return (
         <_BaseMemberLayout>
@@ -54,26 +82,28 @@ const Search: NextPage = () => {
             </Head>
             <div className='flex flex-col px-10 mt-2'>
                 <FormLabel
-                    htmlFor={'search'}
-                    _class='block mb-2 pt-4 px-2text-sm font-medium text-gray-900 dark:text-gray-400'>キーワード</FormLabel>
-                <textarea
-                    name="serach"
-                    id="serach"
-                    cols={20}
-                    rows={2}
-                    className='border border-solid border-sub rounded-lg'
+                    htmlFor={'keyword'}
+                    _class='block mb-2 pt-4 px-2text-sm font-medium text-gray-900 text-main'>キーワード</FormLabel>
+                <input
+                    type='text'
+                    name="keyword"
+                    id="keyword"
+                    className='border border-solid border-sub text-2xl leading-relaxed'
                     onChange={changeHandler}
-                >
-                </textarea>
+                />
             </div>
             <div className='flex  justify-end px-10 mt-2'>
                 <button
-                    className='font-semibold px-5 py-3 rounded-lg bg-sub text-main text'
+                    className='font-semibold px-5 py-3 rounded-lg bg-sub text-main'
                     onClick={onClickSearch}
                 >検索</button>
             </div>
-            <div>
-
+            <div className='grid grid-cols-12 px-6 md:px-20'>
+                {
+                    formcards.map((elem, index) => {
+                        return <SearchResultCard key={index} value1={elem.title} value2={elem.content} id={elem.id} />
+                    })
+                }
             </div>
 
         </_BaseMemberLayout>
