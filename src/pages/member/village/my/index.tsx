a5f684ac405bb6cee00c78fa8e1513b8310b0436
ShunMitsuoka <physics.sm0414@gmail.com>
2022-08-01 15:33:23 +0000

    myビレッジ作成

--- src/pages/member/village/my/index.tsx
+++ src/pages/member/village/my/index.tsx
@@ -5,11 +5,21 @@ import axios from '@/libs/axios/axios';
 import type { NextPage } from 'next'
 import { useSession } from 'next-auth/react';
 import Head from 'next/head'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+type villageType = {
+  id : number,
+  title : string,
+  phase_name : string,
+  content : string,
+  role_id : number,
+}
 
 const MyVillage: NextPage = () => {
 
   const { data: session, status } = useSession();
+
+  const [lists, setLists] = useState<villageType[]>([]);
 
   useEffect(() => {
     if(status === "authenticated"){
@@ -17,13 +27,26 @@ const MyVillage: NextPage = () => {
       .then(function (response) {
           const res = ApiService.makeApiResponse(response);
           if(res.getSuccess()){
+            const result = res.getResult();
+            setLists(result);
             console.log(res);
           }else{
               alert('失敗');
           }
       })
     }
-},[status]);
+  },[status]);
+
+  const getColorClass = (role_id:number) => {
+    switch (role_id) {
+      case 0:
+        return 
+        break;
+    
+      default:
+        break;
+    }
+  }
 
 
   return (
@@ -33,6 +56,18 @@ const MyVillage: NextPage = () => {
       </Head>
       <div className=''>
           参加中ビレッジ一覧
+          <div className=' px-10'>
+            {
+              lists.map((village, index) => {
+                return (
+                  <div key={index} className='bg-white rounded-lg drop-shadow-lg'>
+                    <div className='px-4 py-6 text-center'>{village.title}</div>
+                    <div></div>
+                  </div>
+                );
+              })
+            }
+          </div>
       </div>
   </_BaseMemberLayout>
   )
