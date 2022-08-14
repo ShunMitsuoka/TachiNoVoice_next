f0af92a8c4de77c1b894d20f2a2238f93d368dba
ozkoharu <78905667+ozkoharu@users.noreply.github.com>
2022-08-14 08:24:05 +0000

    コアメンバー意見募集

--- src/pages/member/village/my/details/coreMemberOpinion/[id].tsx
+++ src/pages/member/village/my/details/coreMemberOpinion/[id].tsx
@@ -1,9 +1,15 @@
 import { AuthService } from "@/app/services/authService";
+import { FormLabel } from "@/components/atoms/label/formLabel";
 import { usePageLoading } from "@/hooks/common/usePageLoading";
 import _BaseMemberLayout from "@/layouts/_baseMemberLayout";
 import { NextPage, GetServerSideProps } from "next";
 import { getSession, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import { useState } from "react";
+
+type formData = {
+  userOpinion: string,
+}
 
 const CoreMemberOpinion: NextPage = () => {
 
@@ -11,10 +17,32 @@ const CoreMemberOpinion: NextPage = () => {
   const pageLoading = usePageLoading();
   const router = useRouter();
   const { id } = router.query;
+  const [opinion, setOpinion] = useState<formData>({
+    userOpinion: "",
+  });
+
+  const changeHandler = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setOpinion(prevValues => {
+      return { ...prevValues, [e.target.name]: e.target.value }
+    });
+  }
 
   return (
-    <_BaseMemberLayout pageLoding={pageLoading.isPageLaoding}>
+    <_BaseMemberLayout pageLoding={pageLoading.isPageLaoding} title='コアメンバー意見募集'>
+      <div className='text-center text-sub text-2xl mt-8'>以下の問題について意見してください</div>
 
+      <div className='text-center text-sub text-3xl mt-6'>ここにタイトルを入れます</div>
+      <div className='text-center text-sub text-3xl mt-6'>その下にcontentを入れます</div>
+      <div className='flex flex-col px-10 mt-3'>
+        <FormLabel
+          htmlFor={'opinion'}
+          _class='block mb-2 pt-4 px-2text-sm font-medium text-sub text-gray-900 text-main'>意見欲しいな</FormLabel>
+        <input
+          type='text'
+          name='opinion'
+          id='opinion'
+        />
+      </div>
     </_BaseMemberLayout>
   )
 }
