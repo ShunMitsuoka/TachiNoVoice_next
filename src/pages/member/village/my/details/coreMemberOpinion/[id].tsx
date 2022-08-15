834ca1b17824b6a02048121ade360bf1588577b6
ozkoharu <78905667+ozkoharu@users.noreply.github.com>
2022-08-15 03:43:20 +0000

    コアメンバー意見募集

--- src/pages/member/village/my/details/coreMemberOpinion/[id].tsx
+++ src/pages/member/village/my/details/coreMemberOpinion/[id].tsx
@@ -7,9 +7,7 @@ import { getSession, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useState } from "react";
 
-type formData = {
-  userOpinion: string,
-}
+
 
 const CoreMemberOpinion: NextPage = () => {
 
@@ -17,33 +15,55 @@ const CoreMemberOpinion: NextPage = () => {
   const pageLoading = usePageLoading();
   const router = useRouter();
   const { id } = router.query;
-  const [opinion, setOpinion] = useState<formData>({
-    userOpinion: "",
-  });
+  const [opinion, setOpinion] = useState("");
+  const [now, setNow] = useState<number>(1);
 
-  const changeHandler = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    setOpinion(prevValues => {
-      return { ...prevValues, [e.target.name]: e.target.value }
-    });
+
+  const onClickNext = () => {
+    console.log(opinion);
+    console.log('Next');
+    setNow(1);
+  }
+  const onClickBack = () => {
+    console.log('Back');
+    setNow(0);
   }
 
   return (
     <_BaseMemberLayout pageLoding={pageLoading.isPageLaoding} title='コアメンバー意見募集'>
-      <div className='text-center text-sub text-2xl mt-8'>以下の問題について意見してください</div>
-
-      <div className='text-center text-sub text-3xl mt-6'>ここにタイトルを入れます</div>
-      <div className='text-center text-sub text-3xl mt-6'>その下にcontentを入れます</div>
+      <div className='text-center text-sub text-2xl mt-8'>
+        以下の問題について意見してください
+      </div>
+      <div className='text-center text-sub text-3xl mt-6'>
+        ここにタイトルを入れます
+      </div>
+      <div className='text-center text-sub text-3xl mt-6'>
+        その下にcontentを入れます
+      </div>
       <div className='flex flex-col px-10 mt-3'>
-        <FormLabel
-          htmlFor={'opinion'}
-          _class='block mb-2 pt-4 px-2text-sm font-medium text-sub text-gray-900 text-main'>意見欲しいな</FormLabel>
-        <input
-          type='text'
-          name='opinion'
-          id='opinion'
+        <textarea
+          className="
+          block
+          w-full
+          px-4
+          py-4
+          border border-solid border-sub
+          rounded-lg
+          text-2xl
+          focus: text-gray-700 focus:border-blue-600 focus:outline-none
+          "
+          placeholder="意見を入力してください"
+          rows={10}
+          onChange={(event) => setOpinion(event.target.value)}
         />
       </div>
-    </_BaseMemberLayout>
+      <div>
+        <button className='mt-10 font-semibold px-7 py-3 rounded-lg bg-sub text-main transition ease-in-out'
+          onClick={onClickBack}>戻る</button>
+        <button className='mt-10 font-semibold px-7 py-3 rounded-lg bg-sub text-main transition ease-in-out'
+          onClick={onClickNext}>確定</button>
+      </div>
+    </_BaseMemberLayout >
   )
 }
 
