8013a0743fa999b6d178bbce4100296a6000d6b2
ShunMitsuoka <physics.sm0414@gmail.com>
2022-09-29 07:49:11 +0000

    トップ画面レイアウト調整

--- src/layouts/_baseGuestLayout.tsx
+++ src/layouts/_baseGuestLayout.tsx
@@ -1,6 +1,7 @@
 import { GuestFooter } from "../components/templates/guest/guestFooter";
 import { GuestHeader } from "../components/templates/guest/guestHeader";
 import { NextPage } from "next";
+import Image from 'next/image';
 import _BaseLayout from "./_baseLayout";
 
 type Props = {
@@ -16,8 +17,22 @@ const _BaseGuestLayout: NextPage<Props> = ({
         <_BaseLayout>
             <div className="relative flex flex-col bg-main text-sub font-main min-h-screen">
                 <GuestHeader title={title}/>
-                <div className="flex-1 pb-32">
+                <div className="relative flex-1 pb-32">
+                    <div className="absolute top-2 left-2">
+                        <Image
+                            src={'/images/common/decoration/tl-deco.svg'}
+                            width={300}
+                            height={130}
+                        />
+                    </div>
                     {children}
+                    <div className="absolute bottom-1 right-2">
+                        <Image
+                            src={'/images/common/decoration/br-deco.svg'}
+                            width={300}
+                            height={130}
+                        />
+                    </div>
                 </div>
                 <div className="mt-auto">
                     <GuestFooter />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { RouteManager } from '@/app/manages/routeManager'
 import { LinkButton } from '@/components/atoms/buttons/linkButton'
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
 import Link from 'next/link'
 import _BaseGuestLayout from '../layouts/_baseGuestLayout'
 
@@ -14,6 +15,24 @@ const Home: NextPage = () => {
       </Head>
       <div className='py-16 text-3xl text-center'>
         <h1>TACHI-NO-VOICE</h1>
+      </div>
+      <div className="relative w-full mb-10">
+          <Image
+              src={'/images/guest/top/meeting.jpg'}
+              layout={'responsive'}
+              width={192}
+              height={128}
+          />
+        <div className='absolute top-0 left-0 flex justify-center items-center w-full h-full bg-white bg-opacity-80'>
+          <div className='relative text-center text-xl leading-relaxed'>
+            真のリーダーとは<br />
+            合意を探す者ではなく<br />
+            合意の形成者となる者である<br />
+            <div className='text-lg absolute -bottom-8 right-0'>
+              by  Martin Luther King Jr.
+            </div>
+          </div>
+        </div>
       </div>
       <div className='flex flex-col justify-center'>
         <div className='text-center'>
