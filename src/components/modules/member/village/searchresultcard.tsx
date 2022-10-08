57992c31e1843bd38bb58f50e2422550031c926a
ozkoharu <78905667+ozkoharu@users.noreply.github.com>
2022-10-08 02:55:22 +0000

    contentのいらないとこ消しました

--- src/components/modules/member/village/searchresultcard.tsx
+++ src/components/modules/member/village/searchresultcard.tsx
@@ -14,16 +14,12 @@ export const SearchResultCard: React.FC<Props> = ({
     content = '',
 }) => {
     return (
-        <div className='col-span-12 md:col-span-6'>
+        <div className='col-span-12 md:col-span-6 mt-6'>
             <Link href={RouteManager.webRoute.member.village.search.details + id}>
                 <div className='text-sub text-center rounded-lg drop-shadow bg-white overflow-hidden shadow-lg'>
                     <div className='text-2xl text-center font-bold bg-sub text-white py-3'>{title}</div>
                     <div className='px-4 py-4 overflow-auto'>
                         {content}
-                        aaaaaaajapihpjpajpdeahpaaaaaaajapihpjpa <br />
-                        jpdeahpaaaaaaajapihpjpajpdeahpaaaaaaaja<br />
-                        pihpjpajpdeahpaaaaaaajapihpjpajpdeahpaaaaa<br />
-                        aajapihpjpajpdeahpaaaaaaajapihpjpajpdeahp<br />
                     </div>
                 </div>
             </Link>
