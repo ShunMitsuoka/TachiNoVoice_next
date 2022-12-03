a7d6c9893483c2e1a62ef5cc27c9f3707077058c
ShunMitsuoka <physics.sm0414@gmail.com>
2022-12-03 06:15:35 +0000

    animatio追加

--- src/hooks/common/useModal.tsx
+++ src/hooks/common/useModal.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from 'framer-motion';
 import React, { useState } from 'react';
 
 export const useModal = () => {
@@ -15,12 +16,18 @@ export const useModal = () => {
   const content = (children : React.ReactNode) => {
     return (
         isShow ?
-        <div className='fixed top-0 left-0 flex items-center justify-center w-full h-full bg-slate-800 bg-opacity-60 z-10'>
-            <div className='absolute top-0 left-0 w-full h-full' onClick={close}></div>
-            <div className='relative w-full px-4'>
-            {children}
-            </div>
-        </div>
+        <AnimatePresence>
+          <div className='fixed top-0 left-0 flex items-center justify-center w-full h-full bg-slate-800 bg-opacity-60 z-10'>
+              <div className='absolute top-0 left-0 w-full h-full' onClick={close}></div>
+              <motion.div 
+                className='relative w-full px-4'
+                initial={{ scale: 0.2, opacity: 0 }}
+                animate={{ scale:  1, opacity: 1 }}
+              >
+              {children}
+              </motion.div>
+          </div>
+        </AnimatePresence>
         :
         null
     );
